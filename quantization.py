ad1528f84aec12ddbc8e1a45d3898a2465b59db7
cookiepresence <127502417+cookiepresence@users.noreply.github.com>
2025-03-10 16:13:32 +0530

    feat: move to torchao

--- quantization.py
+++ quantization.py
@@ -1,5 +1,5 @@
 import torch
-import torch.ao.quantization as tq
+import torchao
 import torchvision.models as models
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
@@ -23,17 +23,11 @@ def get_cifar10_subset():
     return DataLoader(torch.utils.data.Subset(dataset, range(100)), batch_size=10)
 
 def apply_static_quantization(model, data_loader):
-    # model.fuse_model()
-    model.qconfig = tq.get_default_qconfig("fbgemm")
-    tq.prepare(model, inplace=True)
-    with torch.no_grad():
-        for inputs, _ in data_loader:
-            model(inputs)
-    tq.convert(model, inplace=True)
+    torchao.quantize_(torch.compile(model, mode='max-autotune'), torchao.quantization.quant_api.Int8WeightOnlyConfig())
     return model
 
-def apply_dynamic_quantization(model):
-    return tq.quantize_dynamic(model, {torch.nn.Linear}, dtype=torch.qint8)
+# def apply_dynamic_quantization(model):
+#     return tq.quantize_dynamic(model, {torch.nn.Linear}, dtype=torch.qint8)
 
 def main(model_path, output_path, quant_type):
     model = load_resnet_model(model_path)
@@ -41,8 +35,8 @@ def main(model_path, output_path, quant_type):
     if quant_type == "static":
         data_loader = get_cifar10_subset()
         model = apply_static_quantization(model, data_loader)
-    else:
-        model = apply_dynamic_quantization(model)
+    # else:
+    #     model = apply_dynamic_quantization(model)
     
     torch.save(model.state_dict(), output_path)
     print(f"Quantized model saved to {output_path}")
@@ -51,7 +45,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", type=str, required=True, help="Path to the pretrained model file", dest='model_path')
     parser.add_argument("--output-path", type=str, required=True, help="Path to save the quantized model", dest='output_path')
-    parser.add_argument("--quant-type", type=str, choices=["static", "dynamic"], required=True, help="Type of quantization", dest='quant_type')
+    parser.add_argument("--quant-type", type=str, choices=["static"], required=True, help="Type of quantization", dest='quant_type')
     args = parser.parse_args()
     
     main(args.model_path, args.output_path, args.quant_type)
