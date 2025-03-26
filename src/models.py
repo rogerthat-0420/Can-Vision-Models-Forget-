fa3ee59a18743fe5a34a78a298afc6bdeb8a3555
AdityaMishraOG <adityamishra34351729@gmail.com>
2025-03-27 01:11:15 +0530

    just formatting

--- src/models.py
+++ src/models.py
@@ -4,13 +4,14 @@ import torch.nn.init as init
 from torchvision.models import resnet50
 from utils import EmbedLayer, Classifier, vit_init_weights
 
+
 class ResNet50(nn.Module):
     def __init__(self, num_classes=10):
         super(ResNet50, self).__init__()
         self.model = resnet50(weights=None)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
-        
-        # initializing the new fc layer properly. 
+
+        # initializing the new fc layer properly.
         init.kaiming_normal_(self.model.fc.weight)
         if self.model.fc.bias is not None:
             init.zeros_(self.model.fc.bias)
@@ -18,9 +19,10 @@ class ResNet50(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+
 class ViT(nn.Module):
     """
-    Vision Transformer Class with Pytorch transformer layers (TransformerEncoder and TransformerEncoderLayer) instead of scratch implementation. 
+    Vision Transformer Class with Pytorch transformer layers (TransformerEncoder and TransformerEncoderLayer) instead of scratch implementation.
     These layer replace the encoder layers (including self-attention operation). Hence, SelfAttention and Encoder classes can be removed.
     Embed layer cannot be replaced as Image to PatchEmbedding Block not available in PyTorch yet.
     Classifier is a simple MLP (and not replaced/not available in PyTorch).
@@ -35,27 +37,45 @@ class ViT(nn.Module):
         patch_size (int)        : Patch size
         n_classes (int)         : Number of classes
         dropout  (float)        : dropout value
-    
+
     Input:
         x (tensor): Image Tensor of shape B, C, IW, IH
 
     Returns:
         Tensor: Logits of shape B, CL
-    """    
-    def __init__(self, n_channels, embed_dim, n_layers, n_attention_heads, forward_mul, image_size, patch_size, n_classes, dropout=0.1):
+    """
+
+    def __init__(
+        self,
+        n_channels,
+        embed_dim,
+        n_layers,
+        n_attention_heads,
+        forward_mul,
+        image_size,
+        patch_size,
+        n_classes,
+        dropout=0.1,
+    ):
         super().__init__()
-        self.embedding  = EmbedLayer(n_channels, embed_dim, image_size, patch_size, dropout=dropout)
-        encoder_layer   = nn.TransformerEncoderLayer(d_model=embed_dim, 
-                                                     nhead=n_attention_heads, 
-                                                     dim_feedforward=forward_mul*embed_dim, 
-                                                     dropout=dropout, 
-                                                     activation=nn.GELU(), 
-                                                     batch_first=True, 
-                                                     norm_first=True)
-        self.encoder    = nn.TransformerEncoder(encoder_layer, n_layers, norm=nn.LayerNorm(embed_dim))
+        self.embedding = EmbedLayer(
+            n_channels, embed_dim, image_size, patch_size, dropout=dropout
+        )
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=embed_dim,
+            nhead=n_attention_heads,
+            dim_feedforward=forward_mul * embed_dim,
+            dropout=dropout,
+            activation=nn.GELU(),
+            batch_first=True,
+            norm_first=True,
+        )
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer, n_layers, norm=nn.LayerNorm(embed_dim)
+        )
         self.classifier = Classifier(embed_dim, n_classes)
 
-        self.apply(vit_init_weights)                                                    # Weight initalization
+        self.apply(vit_init_weights)  # Weight initalization
 
     def forward(self, x):
         x = self.embedding(x)
@@ -63,14 +83,21 @@ class ViT(nn.Module):
         x = self.classifier(x)
         return x
 
+
 def get_model(name, num_classes, args):
-    if name == 'resnet50':
+    if name == "resnet50":
         return ResNet50(num_classes)
-    elif name == 'vit' or name == 'ViT':
-        return ViT(n_channels=args.n_channels,   embed_dim=args.embed_dim, 
-                   n_layers=args.n_layers,       n_attention_heads=args.n_attention_heads, 
-                   forward_mul=args.forward_mul, image_size=args.image_size, 
-                   patch_size=args.patch_size,   n_classes=num_classes, 
-                   dropout=args.dropout)
+    elif name == "vit" or name == "ViT":
+        return ViT(
+            n_channels=args.n_channels,
+            embed_dim=args.embed_dim,
+            n_layers=args.n_layers,
+            n_attention_heads=args.n_attention_heads,
+            forward_mul=args.forward_mul,
+            image_size=args.image_size,
+            patch_size=args.patch_size,
+            n_classes=num_classes,
+            dropout=args.dropout,
+        )
     else:
         raise NotImplementedError(f"Model {name} is not implemented.")
--- src/quantization.py
+++ src/quantization.py
@@ -12,36 +12,51 @@ import torch_tensorrt as torchtrt
 from src.models import ResNet50
 from src.models import ViT
 
+
 def load_resnet_model(model_path):
     model = ResNet50()
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
     model.eval()
     return model
+
 
 def load_vit_model(model_path):
     model = ViT()
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
     model.eval()
     return model
 
+
 def get_cifar10_subset():
-    transform = transforms.Compose([
-        transforms.Resize(224),  # ResNet expects 224x224 inputs
-        transforms.ToTensor(),
-    ])
-    dataset = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
+    transform = transforms.Compose(
+        [
+            transforms.Resize(224),  # ResNet expects 224x224 inputs
+            transforms.ToTensor(),
+        ]
+    )
+    dataset = datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform
+    )
     return DataLoader(torch.utils.data.Subset(dataset, range(256)), batch_size=2)
 
+
 def get_cifar100_subset():
-    transform = transforms.Compose([transforms.Resize([args.image_size, args.image_size]),
-                                            transforms.RandomCrop(args.image_size, padding=4), 
-                                            transforms.RandomHorizontalFlip(),
-                                            transforms.RandAugment(),  # RandAugment augmentation for strong regularization
-                                            transforms.ToTensor(), 
-                                            transforms.Normalize([0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761])])
-    dataset = datasets.CIFAR100(root='../data', train=True, download=True, transform=transform)
+    transform = transforms.Compose(
+        [
+            transforms.Resize([args.image_size, args.image_size]),
+            transforms.RandomCrop(args.image_size, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandAugment(),  # RandAugment augmentation for strong regularization
+            transforms.ToTensor(),
+            transforms.Normalize([0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761]),
+        ]
+    )
+    dataset = datasets.CIFAR100(
+        root="../data", train=True, download=True, transform=transform
+    )
     return DataLoader(torch.utils.data.Subset(dataset, range(256)), batch_size=2)
-    
+
+
 def calculate_model_sizes(model):
     param_size = 0
     for param in model.parameters():
@@ -49,8 +64,9 @@ def calculate_model_sizes(model):
     buffer_size = 0
     for buffer in model.buffers():
         buffer_size += buffer.nelement() * buffer.element_size()
-        
+
     return param_size + buffer_size
+
 
 def calibrate_loop(model, dataloader, loss_fn=torch.nn.functional.cross_entropy):
     # calibrate over the training dataset
@@ -66,6 +82,7 @@ def calibrate_loop(model, dataloader, loss_fn=torch.nn.functional.cross_entropy)
         correct += (preds == labels).sum().item()
         print(f"batch loss: {loss}, batch correct: {correct} / {total}")
     print("PTQ Loss: {:.5f} Acc: {:.2f}%".format(loss / total, 100 * correct / total))
+
 
 # def apply_static_quantization(model, data_loader):
 #     model = model.bfloat16()
@@ -83,50 +100,82 @@ def calibrate_loop(model, dataloader, loss_fn=torch.nn.functional.cross_entropy)
 #     # print(f"model went from {original_size} -> {final_size}")
 #     return model
 
+
 def apply_ptq(model, dataloader, quant_size):
     model = model.cuda()
     if quant_size == "int8":
         quant_cfg = mtq.INT8_DEFAULT_CFG
     elif quant_size == "fp8":
         quant_cfg = mtq.FP8_DEFAULT_CFG
-        
+
     # print accuracy and loss pre-quantization
     calibrate_loop(model, dataloader)
     # PTQ with in-place replacement to quantized modules
-    mtq.quantize(model, quant_cfg, forward_loop=lambda model: calibrate_loop(model, dataloader))
+    mtq.quantize(
+        model, quant_cfg, forward_loop=lambda model: calibrate_loop(model, dataloader)
+    )
     print(model)
     return model
+
 
 def apply_static_quantization(model, dataloader):
     raise NotImplementedError("yet to implement")
 
+
 # def apply_dynamic_quantization(model):
 #     return tq.quantize_dynamic(model, {torch.nn.Linear}, dtype=torch.qint8)
 
+
 def main(model_path, output_path, quant_type, quant_size):
     model = load_vit_model(model_path)
-    
+
     if quant_type == "static":
         # data_loader = get_cifar10_subset()
         data_loader = get_cifar100_subset()
         model = apply_static_quantization(model, data_loader)
-    elif quant_type == 'ptq':
+    elif quant_type == "ptq":
         # data_loader = get_cifar10_subset()
         data_loader = get_cifar100_subset()
         model = apply_ptq(model, data_loader, quant_size)
     # else:
     #     model = apply_dynamic_quantization(model)
-    
+
     mto.save(model, output_path)
     print(f"Quantized model saved to {output_path}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", type=str, required=True, help="Path to the pretrained model file", dest='model_path')
-    parser.add_argument("--output-path", type=str, required=True, help="Path to save the quantized model", dest='output_path')
-    parser.add_argument("--quant-type", type=str, choices=["static", 'ptq'], required=True, help="Type of quantization", dest='quant_type')
-    parser.add_argument("--quant-size", type=str, choices=["fp8", "int8"], required=True, help="what is the final size", dest='quant_size')
+    parser.add_argument(
+        "--model-path",
+        type=str,
+        required=True,
+        help="Path to the pretrained model file",
+        dest="model_path",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        required=True,
+        help="Path to save the quantized model",
+        dest="output_path",
+    )
+    parser.add_argument(
+        "--quant-type",
+        type=str,
+        choices=["static", "ptq"],
+        required=True,
+        help="Type of quantization",
+        dest="quant_type",
+    )
+    parser.add_argument(
+        "--quant-size",
+        type=str,
+        choices=["fp8", "int8"],
+        required=True,
+        help="what is the final size",
+        dest="quant_size",
+    )
     args = parser.parse_args()
-    
+
     main(args.model_path, args.output_path, args.quant_type, args.quant_size)
-    
