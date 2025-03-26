9f467d6098b73a5989382508e4bfc0ead6ed7043
Sumitkk10 <messikumar66@gmail.com>
2025-03-27 02:43:12 +0530

    added ViT impl in exp1.py

--- src/exp1.py
+++ src/exp1.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader, Subset
 from tqdm import tqdm
 from parse import get_args
 from models import get_model
-from utils import load_dataset, train, poison_dataset
+from utils import load_dataset, train, poison_dataset, plot_graphs
 from evaluate import evaluate_model
 from unlearn import PotionUnlearner, FlexibleUnlearner
 
@@ -49,7 +49,6 @@ def train_clean(
     print(f"Final Test Evaluation: {test_metrics}")
 
     return model
-
 
 def train_gold(
     args,
@@ -95,6 +94,64 @@ def train_gold(
     print(f"Final Test Evaluation: {test_metrics}")
     return gold_model
 
+def train_vit(
+        args, 
+        model, 
+        train_loader, 
+        val_loader, 
+        test_loader, 
+        criterion, 
+        device):
+    # Define optimizer for training the model
+    optimizer = optim.AdamW(model.parameters(), lr=args.vit_lr, weight_decay=1e-3)
+
+    # scheduler for linear warmup of lr and then cosine decay to 1e-5
+    linear_warmup = optim.lr_scheduler.LinearLR(optimizer, start_factor=1/args.warmup_epochs, end_factor=1.0, total_iters=args.warmup_epochs-1, last_epoch=-1, verbose=True)
+    cos_decay     = optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=args.og_epochs-args.warmup_epochs, eta_min=1e-5, verbose=True)
+
+    train_losses     = []
+    val_losses      = []
+    train_accuracies = []
+    val_accuracies  = []
+
+    best_loss = float('inf')
+    epochs_no_improve = 0
+
+    for epoch in range(1, args.og_epochs + 1):
+
+        train_loss, train_acc = train(model, train_loader, optimizer, criterion, device)
+        val_metrics = evaluate_model(model, val_loader, device)
+        val_loss = val_metrics['loss']
+        val_acc = val_metrics['accuracy']
+        print(f'Train Loss: {train_loss:.4f}, Train Acc: {train_acc * 100:.4f} | Validation Loss: {val_loss:.4f}, Validation Acc: {val_acc:.4f}')
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+        train_accuracies.append(train_acc)
+        val_accuracies.append(val_acc)
+
+        if epoch < args.warmup_epochs:
+            linear_warmup.step()
+        else:
+            cos_decay.step()
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            epochs_no_improve = 0
+            os.makedirs('../models', exist_ok=True)
+            torch.save(model.state_dict(), f'../models/clean_{args.model}_{args.dataset}.pth')
+        else:
+            epochs_no_improve += 1
+
+        # Early stopping
+        if epochs_no_improve >= args.early_stopping_patience:
+            print(f"Early stopping triggered after {epoch} epochs.")
+            break
+    
+    test_metrics = evaluate_model(model, test_loader, device)
+    print(f"Final Test Evaluation: {test_metrics}")
+
+    return model, train_losses, val_losses, train_accuracies, val_accuracies
 
 if __name__ == "__main__":
 
@@ -113,7 +170,7 @@ if __name__ == "__main__":
     print(
         f"Train size={len(train_dataset)}, Val size={len(val_dataset)}, Test size={len(test_dataset)}"
     )
-    clean_model = get_model(args.model, num_classes=num_classes).to(device)
+    clean_model = get_model(args.model, num_classes=num_classes, args=args).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.AdamW(
         clean_model.parameters(),
@@ -121,19 +178,32 @@ if __name__ == "__main__":
         weight_decay=args.og_weight_decay,
     )
 
+    n_parameters = sum(p.numel() for p in clean_model.parameters() if p.requires_grad)
+    print(f"Number of trainable parameters in the model: {n_parameters}")
+
     # TRAINING PIPELINE
     if args.train_clean:
         print("==== Training Original Model on Clean Dataset ====")
-        clean_model = train_clean(
-            args,
-            clean_model,
-            train_loader,
-            val_loader,
-            test_loader,
-            optimizer,
-            criterion,
-            device,
-        )
+        if(args.model == 'ViT'):
+            (
+                clean_model, 
+                train_losses, 
+                val_losses, 
+                train_accuracies, 
+                val_accuracies
+            ) = train_vit(args, clean_model, train_loader, val_loader, test_loader, criterion, device)
+            plot_graphs(train_losses, val_losses, train_accuracies, val_accuracies)
+        else:
+            clean_model = train_clean(
+                args,
+                clean_model,
+                train_loader,
+                val_loader,
+                test_loader,
+                optimizer,
+                criterion,
+                device,
+            )
 
     else:
         print("==== Loading Original Model ====")
@@ -142,8 +212,9 @@ if __name__ == "__main__":
                 f"../models/clean_{args.model}_{args.dataset}.pth", map_location=device
             )
         )
-    # metrics = evaluate_model(clean_model, test_loader, device)
-    # print(f"OG Evaluation: {metrics}")
+    
+    metrics = evaluate_model(clean_model, test_loader, device)
+    print(f"OG Evaluation: {metrics}")
 
     (
         forget_idx,
@@ -200,7 +271,8 @@ if __name__ == "__main__":
     )
 
     # GOLD STANDARD
-    gold_model = get_model("resnet50", num_classes=100).to(device)
+    print("==== Training Gold Standard Model ====")
+    gold_model = get_model(args.model, num_classes=100, args=args).to(device)
     gold_model = train_gold(
         args,
         gold_model,
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,94 @@
 import torch
+import torch.nn as nn
 from torch.utils.data import random_split, DataLoader, Subset
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from tqdm import tqdm
 import random
+import matplotlib.pyplot as plt
 
+class EmbedLayer(nn.Module):
+    """
+    Class for Embedding an Image.
+    It breaks image into patches and embeds patches using a Conv2D Operation (Works same as the Linear layer).
+    Next, a learnable positional embedding vector is added to all the patch embeddings to provide spatial position.
+    Finally, a classification token is added which is used to classify the image.
+
+    Parameters:
+        n_channels (int) : Number of channels of the input image
+        embed_dim  (int) : Embedding dimension
+        image_size (int) : Image size
+        patch_size (int) : Patch size
+        dropout  (float) : dropout value
+
+    Input:
+        x (tensor): Image Tensor of shape B, C, IW, IH
+    
+    Returns:
+        Tensor: Embedding of the image of shape B, S, E
+    """    
+    def __init__(self, n_channels, embed_dim, image_size, patch_size, dropout=0.0):
+        super().__init__()
+        self.conv1         = nn.Conv2d(n_channels, embed_dim, kernel_size=patch_size, stride=patch_size)                       # Patch Encoding
+        self.pos_embedding = nn.Parameter(torch.zeros(1, (image_size // patch_size) ** 2, embed_dim), requires_grad=True)      # Learnable Positional Embedding
+        self.cls_token     = nn.Parameter(torch.zeros(1, 1, embed_dim), requires_grad=True)                                    # Classification Token
+        self.dropout       = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B = x.shape[0]
+        x = self.conv1(x)                                                         # B, C, IH, IW     --> B, E, IH/P, IW/P                Split image into the patches and embed patches
+        x = x.reshape([B, x.shape[1], -1])                                        # B, E, IH/P, IW/P --> B, E, (IH/P*IW/P) --> B, E, N    Flattening the patches
+        x = x.permute(0, 2, 1)                                                    # B, E, N          --> B, N, E                         Rearrange to put sequence dimension in the middle
+        x = x + self.pos_embedding                                                # B, N, E          --> B, N, E                         Add positional embedding
+        x = torch.cat((torch.repeat_interleave(self.cls_token, B, 0), x), dim=1)  # B, N, E          --> B, (N+1), E       --> B, S, E    Add classification token at the start of every sequence
+        x = self.dropout(x)
+        return x
+    
+class Classifier(nn.Module):
+    """
+    Classification module of the Vision Transformer. Uses the embedding of the classification token to generate logits.
+
+    Parameters:
+        embed_dim (int) : Embedding dimension
+        n_classes (int) : Number of classes
+    
+    Input:
+        x (tensor): Tensor of shape B, S, E
+
+    Returns:
+        Tensor: Logits of shape B, CL
+    """    
+
+    def __init__(self, embed_dim, n_classes):
+        super().__init__()
+        # New architectures skip fc1 and activations and directly apply fc2.
+        self.fc1        = nn.Linear(embed_dim, embed_dim)
+        self.activation = nn.Tanh()
+        self.fc2        = nn.Linear(embed_dim, n_classes)
+
+    def forward(self, x):
+        x = x[:, 0, :]              # B, S, E --> B, E          Get CLS token
+        x = self.fc1(x)             # B, E    --> B, E
+        x = self.activation(x)      # B, E    --> B, E    
+        x = self.fc2(x)             # B, E    --> B, CL
+        return x
+    
+def vit_init_weights(m): 
+    """
+    function for initializing the weights of the Vision Transformer.
+    """    
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        nn.init.trunc_normal_(m.weight, mean=0.0, std=0.02)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+
+    elif isinstance(m, EmbedLayer):
+        nn.init.trunc_normal_(m.cls_token, mean=0.0, std=0.02)
+        nn.init.trunc_normal_(m.pos_embedding, mean=0.0, std=0.02)
 
 class PoisonedDataset(torch.utils.data.Dataset):
     def __init__(self, base_dataset, poisoned_labels=None):
@@ -22,7 +106,6 @@ class PoisonedDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.base_dataset)
-
 
 def load_dataset(args, device):
     torch.manual_seed(args.seed)
@@ -52,7 +135,24 @@ def load_dataset(args, device):
         ]
     )
 
-    if args.dataset == "cifar10":
+    if args.model == "ViT":
+        train_transform = transforms.Compose([transforms.Resize([args.image_size, args.image_size]),
+                                            transforms.RandomCrop(args.image_size, padding=4), 
+                                            transforms.RandomHorizontalFlip(),
+                                            transforms.RandAugment(),  # RandAugment augmentation for strong regularization
+                                            transforms.ToTensor(), 
+                                            transforms.Normalize([0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761])])
+        dataset = datasets.CIFAR100(root='../data', train=True, download=True, transform=train_transform)
+
+        test_transform = transforms.Compose([transforms.Resize([args.image_size, args.image_size]), 
+                                             transforms.ToTensor(), 
+                                             transforms.Normalize([0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761])])
+        test_dataset = datasets.CIFAR100(root='../data', train=False, download=True, transform=test_transform)
+        if args.dataset == 'cifar100':
+            num_classes=100
+
+
+    elif args.dataset == "cifar10":
         dataset = datasets.CIFAR10(
             root="../data", train=True, download=True, transform=transform
         )
@@ -120,7 +220,6 @@ def load_dataset(args, device):
         test_dataset,
     )
 
-
 def train(model, train_loader, optimizer, criterion, device):
     model.train()
     total_loss, correct, total_samples = 0, 0, 0
@@ -138,7 +237,6 @@ def train(model, train_loader, optimizer, criterion, device):
         total_samples += labels.size(0)
 
     return total_loss / len(train_loader), correct / total_samples
-
 
 def poison_dataset(args, train_dataset, val_dataset, test_dataset):
     torch.manual_seed(args.seed)
@@ -241,3 +339,33 @@ def poison_dataset(args, train_dataset, val_dataset, test_dataset):
         test_forget_idx,
         test_retain_idx,
     )
+
+def plot_graphs(train_losses, test_losses, train_accuracies, test_accuracies):
+    # Plot graph of loss values
+    plt.plot(train_losses, color='b', label='Train')
+    plt.plot(test_losses, color='r', label='Test')
+
+    plt.ylabel('Loss', fontsize = 18)
+    plt.yticks(fontsize=16)
+    plt.xlabel('Epoch', fontsize = 18)
+    plt.xticks(fontsize=16)
+    plt.legend(fontsize=15, frameon=False)
+
+    # plt.show()  # Uncomment to display graph
+    plt.savefig('../imgs/graph_loss.png', bbox_inches='tight')
+    plt.close('all')
+
+
+    # Plot graph of accuracies
+    plt.plot(train_accuracies, color='b', label='Train')
+    plt.plot(test_accuracies, color='r', label='Test')
+
+    plt.ylabel('Accuracy', fontsize = 18)
+    plt.yticks(fontsize=16)
+    plt.xlabel('Epoch', fontsize = 18)
+    plt.xticks(fontsize=16)
+    plt.legend(fontsize=15, frameon=False)
+
+    # plt.show()  # Uncomment to display graph
+    plt.savefig('../imgs/graph_accuracy.png', bbox_inches='tight')
+    plt.close('all')
